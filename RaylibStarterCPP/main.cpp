fba0187bb5dec4b81a6d83895ff7c9d3565771cf
Deion Trinidad <deion_trinidad@outlook.com>
2023-08-17 12:46:56 +1000

    Adding collision check using x pos, y pos and radius of the ball

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -20,6 +20,17 @@ void Update()
 {
     x += speed_x;
     y += speed_y;
+
+    // Checking for collision with the edges
+    // Using the x pos, y pos and radius of ball to see if ball is colliding with an edge
+    if(y + radius >= GetScreenHeight() || y - radius <= 0) 
+    {
+        speed_y *= -1;
+    }
+    if (x + radius >= GetScreenWidth() || x - radius <= 0)
+    {
+        speed_x *= -1;
+    }
 }
 
 };
