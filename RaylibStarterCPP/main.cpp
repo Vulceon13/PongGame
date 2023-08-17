0148d22a7cc51313da173f7ea1b3e07d8362dd97
Deion Trinidad <deion_trinidad@outlook.com>
2023-08-17 12:34:50 +1000

    Adding movement to the ball

--- RaylibStarterCPP/main.cpp
+++ RaylibStarterCPP/main.cpp
@@ -4,25 +4,52 @@
 #define RAYGUI_SUPPORT_ICONS
 #include "raygui.h"
 
+class Ball 
+{
+public:
+float x, y;
+int speed_x, speed_y;
+int radius;
+
+void Draw() 
+{
+    DrawCircle(x, y, radius, WHITE); // Drawing Ball
+}
+
+void Update()
+{
+    x += speed_x;
+    y += speed_y;
+}
+
+};
+
+Ball ball;
+
 int main(int argc, char* argv[])
 {
     int screenWidth = 1280;
     int screenHeight = 800;
     InitWindow(screenWidth, screenHeight, "Pong!");
     SetTargetFPS(60);
-    
 
+    ball.radius = 20;
+    ball.x = screenWidth/2;
+    ball.y = screenHeight / 2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
+    
     while (!WindowShouldClose())   
     {    
         BeginDrawing();
 
-        ClearBackground(BLACK); // Creating Black Background
+        //Updating
+        ball.Update();
 
-        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE); // Drawing A Halfway Line
-        
         //Drawing
-        DrawCircle(screenWidth/2, screenHeight/2, 20, WHITE); // Drawing Ball
-        
+        ClearBackground(BLACK); // Creating Black Background
+        DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, WHITE); // Drawing A Halfway Line                
+        ball.Draw();
         DrawRectangle(10, screenHeight / 2 - 60, 25, 120, WHITE); // Drawing Left Paddle
         DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE); // Drawing Right Paddle
         
